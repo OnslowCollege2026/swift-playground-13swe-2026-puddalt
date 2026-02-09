2107fec02ee4378a4360ed3d3e31627faecf777b
Luna <puddalt@gmail.com>
2026-02-09 01:48:02 +0000

    commiting swift hate

--- Sources/SwiftPlayground/SwiftPlayground.swift
+++ Sources/SwiftPlayground/SwiftPlayground.swift
@@ -4,6 +4,21 @@
 @main
 struct SwiftPlayground {
     static func main() {
-        print("Hello, world!")
+        var total = 0.00
+        let daily_spending = 9.00
+        let total_max = 10.00
+        let lunches = [6.50, 8.00, 5.75, 9.20, 7.10]
+        let days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+
+        for (day, cost) in lunches.enumerated() 
+        {
+            var over = ""
+            if cost > daily_spending {over = "[Over spending]"}
+            print("\(days[day]): $\(cost) \(over)")
+        }
+        for items in lunches {total += items}
+        print("Week total: $\(total)")
+        if total > total_max {print("Cost over total")
+        }
     }
 }
